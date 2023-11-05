ae3ec26300b533f2203911001505f3c48848ff78
Cristiano <cs.almeida@protonmail.ch>
2023-11-05 22:17:34 +0000

    Memory allocation

---
+++ source/malloc/main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  char *source = "hello";
+  char *target =  malloc(strlen(source) + 1);
+  if (target == NULL) {
+    return 1;
+  }
+
+  // Copy the string in source to target:
+  strcpy(target, source);
+
+  // Set the first character to be uppercase.
+  if (strlen(target) > 0) {
+    target[0] = toupper(target[0]);
+  }
+
+  printf("source: %s\n", source);
+  printf("target: %s\n", target);
+
+  return 0;
+}
