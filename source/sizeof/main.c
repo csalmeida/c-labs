e1bc98cc75ff06e1fcc1f38def78cb5619c6d82a
Cristiano <cs.almeida@protonmail.ch>
2023-11-28 22:02:23 +0000

    Converts size in bytes to bits

--- source/sizeof/main.c
+++ source/sizeof/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int to_bits(int bytes) {
+  int bits = bytes * 8;
+  return bits; 
+}
+
 int main(void) {
   char c = 'a';
   short s = 0xbeef;
@@ -8,10 +13,11 @@ int main(void) {
   long l = 100000000L;
   long long ll = 60000000000LL;
   
-  printf("A char is %lu bytes\n", sizeof(c));
-  printf("A short is %lu bytes\n", sizeof(s));
-  printf("An int is %lu bytes\n", sizeof(i));
-  printf("A long is %lu bytes\n", sizeof(l));
-  printf("A long long is %lu bytes\n", sizeof(ll));
+  printf("A char is %lu bytes and %d bits\n", sizeof(c), to_bits(sizeof(c)));
+  printf("A short is %lu bytes and %d bits\n", sizeof(s), to_bits(sizeof(s)));
+  printf("An int is %lu bytes and %d bits\n", sizeof(i), to_bits(sizeof(i)));
+  printf("A long is %lu bytes and %d bits\n", sizeof(l), to_bits(sizeof(l)));
+  printf("A long long is %lu bytes and %d bits \n", sizeof(ll), to_bits(sizeof(ll)));
+
   return EXIT_SUCCESS;
 }
