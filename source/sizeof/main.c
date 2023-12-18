30fb95becf1b7d8c9cb375e1ad83837e1e38a03a
Cristiano <cs.almeida@protonmail.ch>
2023-12-18 14:11:26 +0000

    Added float to sizeof.

--- source/sizeof/main.c
+++ source/sizeof/main.c
@@ -11,12 +11,14 @@ int main(void) {
   char c = 'a';
   short s = 0xbeef;
   int i = 100000;
+  float f = 3.14159;
   long l = 100000000L;
   long long ll = 60000000000LL;
   
   printf("A char is %lu bytes and %d bits\n", sizeof(c), to_bits(sizeof(c)));
   printf("A short is %lu bytes and %d bits\n", sizeof(s), to_bits(sizeof(s)));
   printf("An int is %lu bytes and %d bits\n", sizeof(i), to_bits(sizeof(i)));
+  printf("An float is %lu bytes and %d bits\n", sizeof(i), to_bits(sizeof(f)));
   printf("A long is %lu bytes and %d bits\n", sizeof(l), to_bits(sizeof(l)));
   printf("A long long is %lu bytes and %d bits \n", sizeof(ll), to_bits(sizeof(ll)));
 
