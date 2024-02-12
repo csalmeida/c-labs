2a4e2377c67d881c1ebe5f73f4c52a2b71fbf4d4
Cristiano <cs.almeida@protonmail.ch>
2024-02-12 01:07:11 +0000

    Fixes to rolling dice media calculator.

--- source/die_rolling_median/main.c
+++ source/die_rolling_median/main.c
@@ -1,18 +1,20 @@
-//Say that you want to simulate rolling a die (singular of dice) a fixed number of times and to compute and print the average value for the die rolls. What control structure would work best for this problem, and why?
+// Say that you want to simulate rolling a die (singular of dice) a fixed number
+// of times and to compute and print the average value for the die rolls. What
+// control structure would work best for this problem, and why?
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <time.h>
 
-uint8_t roll_die(); 
+uint8_t roll_die();
 double roll_die_average(uint8_t times);
 
 int main() {
   // Generates a seed for the random number.
   srand(time(NULL));
 
-  uint8_t times = 232;
-  double median = roll_die_average(232);
+  uint8_t times = 255;
+  double median = roll_die_average(times);
   printf("🎲 Rolled %d times\n🎯 Roll median: %f\n", times, median);
 
   return EXIT_SUCCESS;
@@ -23,24 +25,23 @@ uint8_t roll_die() {
   uint8_t range_min = 1;
   uint8_t range_max = 6 + 1; // Adds 1 to guarantee that 6 be yielded.
 
-  // Because rand() will return any from 0 to 32767.
+  // Because rand() will return any number from 0 to 32767.
   // It's limited to the die's range using the following formula:
-  uint8_t random_num = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;
+  uint8_t random_num =
+      ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;
   return random_num;
 }
 
-// Takes an empty rolls array and the amount of rolls.
-// Each time it rolls it will add an entry to the array.
-// The amount of times depends on the size of the array.
+// Takes the amount of times to roll a die.
+// Sums the roll results together and divides by the times.
 double roll_die_average(uint8_t times) {
   int64_t sum = 0;
 
   // Add all the rolls up:
-  for (size_t index = 0; index < times; index++)
-  {
-    sum = roll_die();
+  for (size_t index = 0; index < times; index++) {
+    sum = sum + roll_die();
   }
 
-  double median = (double) sum / (double) times;
+  double median = (double)sum / (double)times;
   return median;
 }
