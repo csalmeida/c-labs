4a439d2a9c6d62007424c0bbca36acf07c171d99
Cristiano <cs.almeida@protonmail.ch>
2024-04-18 10:54:14 -0100

    Adds comments to compare strings.

--- source/compare_strings/main.c
+++ source/compare_strings/main.c
@@ -5,12 +5,14 @@ int main(void) {
   char *s = "Hello";
   char *t = "Hello";
 
+  // Compares references to pointers, not the contents of the string.
   if (s == t) {
     printf("Same\n");
   } else {
     printf("Different\n");
   }
 
+  // Compares actual string.
   if (strcmp(s, t) == 0) {
     printf("Same\n");
   } else {
